9619ae1e36a552f89dc8333701c6543bd05e438b
Cris Bettis <apt142@gmail.com>
2013-11-09 19:07:51 -0500

    Added require.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+/** Main js file for Dog Spotter */
 
+// Require this stuff
+require(["../bower_components/modernizr/modernizr"], function(modernizr) {
+    // On init stuff for modernizr
+});
+
+require(["../bower_components/jquery/jquery"], function(jquery) {
+    // On init stuff for jquery
+
+    require(["vendor/bootstrap"], function(bootstrap) {
+        // On init stuff for bootstrap
+    });
+
+    require(["../bower_components/underscore/underscore"], function(underscore) {
+        // On init stuff for underscore
+
+        require(["../bower_components/backbone/backbone"], function(backbone) {
+            // On init stuff for backbone
+            /* TODO: Resolve this
+            require(["../bower_components/marionette/marionette"], function(marionette) {
+                // On init stuff for marionette
+            });
+            */
+        });
+    });
+});
